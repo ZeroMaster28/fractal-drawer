eb7b0406e04b073b569c2a53720b59b4be2cb395
Michael <michalsobieraj966@wp.pl>
2021-01-06 00:39:08 +0100

    Added application main class

---
+++ src/app/MainClass.java
@@ -0,0 +1,13 @@
+package app;
+
+import app.frames.MainFrame;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+
+    public static void main(String[] argv) {
+        JFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+}
