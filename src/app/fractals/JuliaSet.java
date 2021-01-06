a5f2fee2a66fd4def49fa1f0456976c9fb064e53
Michael <michalsobieraj966@wp.pl>
2021-01-06 17:59:41 +0100

    Added first fractal implementation: Julia Set

---
+++ src/app/fractals/JuliaSet.java
@@ -0,0 +1,24 @@
+package app.fractals;
+
+public class JuliaSet implements Fractal {
+
+    private final int THRESHOLD = 200;
+    private final ComplexNumber setParameter;
+
+    public JuliaSet(ComplexNumber parameter) {
+        this.setParameter = parameter;
+    }
+
+    @Override
+    public double getPointAcceptanceRatio(ComplexNumber toCheck) {
+        ComplexNumber c = new ComplexNumber(toCheck.getRe(), toCheck.getIm());
+        for(int i=0; i<THRESHOLD; i++) {
+            c.square();
+            c.add(setParameter);
+            if(c.getModulus() > 1000) {
+                return i/(double) THRESHOLD;
+            }
+        }
+        return 1.0;
+    }
+}
