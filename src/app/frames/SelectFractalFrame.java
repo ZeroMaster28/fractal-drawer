09f3b4ea82d69db17c54ba4d1445669f9a355f36
Michael <michalsobieraj966@wp.pl>
2021-01-06 18:43:32 +0100

    Fractal selection frame default info

--- src/app/frames/SelectFractalFrame.java
+++ src/app/frames/SelectFractalFrame.java
@@ -1,6 +1,6 @@
 package app.frames;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  * Class for options with selecting fractals.
@@ -13,5 +13,11 @@ public class SelectFractalFrame extends JFrame {
         setSize(500, 200);
         setLocation(50,50);
         setResizable(false);
+        JLabel label = new JLabel();
+        label.setText("<html> The default fractal is <b>Julia Set</b>.</br> " +
+                "It contains all complex numbers <number>p</number> that satisfy the equation:<br/>" +
+                "<code>z_{n+1} = z_{n}^{2} + c, z_{0} = p and {zn} has its limit.<br/></code>" +
+                "Variable <number>c<number> is an arbitrary number you can play with.</html>");
+        add(label);
     }
 }
